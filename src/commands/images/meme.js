9d29356b1a2e204e09c3afc75902d9208590d344
TenCreator <lucakinsey@gmail.com>
2023-11-07 12:59:05 +0000

    Update meme.js

--- src/commands/images/meme.js
+++ src/commands/images/meme.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const Discord = require('discord.js');
 module.exports = async (client, interaction, args) => {
 
-    fetch(`https://www.reddit.com/r/memes` + `.json?sort=top&t=week&limit=100`).then(res => res.json()).then(async (json) => {
+    fetch(`https://www.reddit.com/r/memes` + `.json?sort=top&t=week`).then(res => res.json()).then(async (json) => {
         let i = Math.floor(Math.random() * json.data.children.length)
         let image = json.data.children[i].data.url
         let caption = json.data.children[i].data.title
